6e81351117f0fcbb5ac70f591414c0699c9dc81c
Muhammad Anas Darwish <muhammadanasdarwish1@gmail.com>
2026-01-01 23:20:58 +0300

    Install Spatie Permission

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponses;
+use App\Traits\ApplyPermissions;
+
 abstract class Controller
 {
-    //
+    use ApplyPermissions, ApiResponses;
 }
---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission as SpatiePermission;
+use Spatie\Permission\Models\Role as SpatieRole;
+
+class PermissionSeeder extends Seeder
+{
+    private $permissionGroups = [
+        'roles' => ['list', 'show', 'create', 'edit', 'delete', 'get-all-permissions'],
+        'training_categories' => ['list', 'show', 'create', 'edit', 'delete'],
+        'health_warnings' => ['list', 'show', 'create', 'edit', 'delete'],
+    ];
+
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Create permissions
+        foreach ($this->permissionGroups as $group => $permissions) {
+            foreach ($permissions as $permission) {
+                SpatiePermission::firstOrCreate([
+                    'name' => "{$group}.{$permission}",
+                    'guard_name' => 'sanctum'
+                ]);
+            }
+        }
+
+        // Create roles and assign permissions
+        $this->createRolesWithPermissions();
+    }
+
+    protected function createRolesWithPermissions()
+    {
+        // Super Admin - gets all permissions for this guard
+        $superAdmin = SpatieRole::firstOrCreate([
+            'name' => 'super-admin',
+            'guard_name' => 'sanctum'
+        ]);
+
+        // Assign only permissions that belong to this guard
+        $superAdmin->givePermissionTo(
+            SpatiePermission::all()
+        );
+    }
+}
