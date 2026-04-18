bd35ee8b0073262f1a4a88ea523fbd366385d5d5
Muhammad Anas Darwish <muhammadanasdarwish1@gmail.com>
2026-04-18 20:26:53 +0300

    Fix countries and cities

--- Modules/Core/SubModules/Location/Routes/api.php
+++ Modules/Core/SubModules/Location/Routes/api.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Modules\Core\SubModules\Location\Http\Controllers\CityController;
 use Modules\Core\SubModules\Location\Http\Controllers\CountryController;
 
-// Public routes
-Route::apiResource('cities', CityController::class)->except(['store', 'update', 'destroy']);
-Route::apiResource('countries', CountryController::class)->except(['store', 'update', 'destroy']);
-
 // Protected routes - Dashboard
 Route::middleware(['auth:sanctum'])->prefix('dashboard')->group(function () {
     Route::apiResource('cities', CityController::class);
